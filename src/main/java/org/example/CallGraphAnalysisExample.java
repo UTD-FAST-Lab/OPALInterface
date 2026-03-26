6bf5f22007d51517fdba2704ade52d7bf3e2721d
mohPYdev <rafieiyan.moh@gmail.com>
2026-03-26 12:37:55 -0500

    changed config file instead of code itself

--- src/main/java/org/example/CallGraphAnalysisExample.java
+++ src/main/java/org/example/CallGraphAnalysisExample.java
@@ -23,27 +23,38 @@ import org.opalj.tac.cg.MTACallGraphKey$;
 import org.opalj.tac.cg.TypeBasedPointsToCallGraphKey$;
 
 import org.opalj.log.LogContext;
+import org.opalj.log.StandardLogContext;
+import org.opalj.log.OPALLogger$;
+import org.opalj.log.Info$;
+import org.opalj.log.ConsoleOPALLogger;
 import org.opalj.log.GlobalLogContext;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.security.auth.login.LoginContext;
-
 
 
 public class CallGraphAnalysisExample {
 
     public static void main(String[] args) {
+
+                        // Forces OPAL to run synchronously, eliminating all thread race crashes
+        // System.setProperty("org.opalj.threads.CPUBoundTasks", "1");
+        // System.setProperty("org.opalj.threads.IOBoundTasks", "1");
         if (args.length != 4) {
             System.err.println("Usage: CallGraphAnalysisExample <app-dir> <output-file> <algorithm> <tamiflex-log>");
             System.exit(1);
         }
 
+        boolean use_tamiflex = true;
         String tamiflexLogPath = args[3];
+        if (tamiflexLogPath.equals("None")){
+            use_tamiflex = false;
+            System.out.println("not using tamiflex");
+        }
 
         // String pathToJar = args[0];
         // String output = args[1];
@@ -99,7 +110,13 @@ public class CallGraphAnalysisExample {
 
         ArrayList<File> libraryJars = new ArrayList<>();
         File jdkJar = new File(JDK_path);
+        if (!jdkJar.exists()) {
+            System.err.println("FATAL: Cannot find Java 8 rt.jar at " + JDK_path);
+            System.err.println("OPAL will crash with a NullPointerException without this file.");
+            System.exit(1);
+        }
         libraryJars.add(jdkJar);
+        libraryJars.add(new File("/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/jce.jar"));
 
         File[] appFiles = applicationJars.toArray(new File[0]);
         File[] libFiles = libraryJars.toArray(new File[0]);
@@ -113,59 +130,100 @@ public class CallGraphAnalysisExample {
 
         // --- Configuration Setup ---
         // Base configuration
-        Config baseConfig = ConfigFactory.load().withValue(
-            "org.opalj.br.reader.ClassFileReader.Invokedynamic.rewrite",
-            ConfigValueFactory.fromAnyRef(true)
-        );
+        Config baseConfig = ConfigFactory.load();
+        // Config baseConfig = ConfigFactory.load().withValue(
+        //     "org.opalj.br.reader.ClassFileReader.Invokedynamic.rewrite",
+        //     ConfigValueFactory.fromAnyRef(true)
+        // );
 
-        List<? extends ConfigObject> existingEntryPoints =
-            baseConfig.getObjectList("org.opalj.br.analyses.cg.InitialEntryPointsKey.entryPoints");
+        // List<? extends ConfigObject> existingEntryPoints =
+        //     baseConfig.getObjectList("org.opalj.br.analyses.cg.InitialEntryPointsKey.entryPoints");
 
 
         // Define the new entry point
-        Map<String, String> mainEntryPoint = new HashMap<>();
-        mainEntryPoint.put("declaringClass", "Harness");
-        mainEntryPoint.put("name", "main");
+        // Map<String, String> mainEntryPoint = new HashMap<>();
+        // mainEntryPoint.put("declaringClass", "Harness");
+        // mainEntryPoint.put("name", "main");
 
-        List<ConfigValue> updatedEntryPoints = new ArrayList<>(existingEntryPoints);
-        updatedEntryPoints.add(ConfigValueFactory.fromMap(mainEntryPoint));
+        // // List<ConfigValue> updatedEntryPoints = new ArrayList<>(existingEntryPoints);
+        // List<ConfigValue> updatedEntryPoints = new ArrayList<>();
+        // updatedEntryPoints.add(ConfigValueFactory.fromMap(mainEntryPoint));
+        
+
 
 
         // Configure the initial entry points
-        Config config;
-        config = baseConfig
-            .withValue(
-                    "org.opalj.br.analyses.cg.InitialEntryPointsKey.analysis",
-                    ConfigValueFactory.fromAnyRef("org.opalj.br.analyses.cg.ConfigurationEntryPointsFinder")
-                    // ConfigValueFactory.fromAnyRef("org.opalj.br.analyses.cg.ApplicationWithoutJREEntryPointsFinder")
-            )
-            .withValue(
-                    "org.opalj.br.analyses.cg.InitialEntryPointsKey.entryPoints",
-                    ConfigValueFactory.fromIterable(updatedEntryPoints)
-            )
-            .withValue(
-                    "org.opalj.br.analyses.cg.InitialInstantiatedTypesKey.analysis",
-                    ConfigValueFactory.fromAnyRef("org.opalj.br.analyses.cg.ApplicationInstantiatedTypesFinder")
-            );
+        // Config config;
+        // config = baseConfig
+        //     .withValue(
+        //             "org.opalj.br.analyses.cg.InitialEntryPointsKey.analysis",
+        //             ConfigValueFactory.fromAnyRef("org.opalj.br.analyses.cg.ConfigurationEntryPointsFinder")
+        //             // ConfigValueFactory.fromAnyRef("org.opalj.br.analyses.cg.ApplicationWithoutJREEntryPointsFinder")
+        //     )
+        //     .withValue(
+        //             "org.opalj.br.analyses.cg.InitialEntryPointsKey.entryPoints",
+        //             ConfigValueFactory.fromIterable(updatedEntryPoints)
+        //     )
+        //     .withValue(
+        //             "org.opalj.br.analyses.cg.InitialInstantiatedTypesKey.analysis",
+        //             ConfigValueFactory.fromAnyRef("org.opalj.br.analyses.cg.ApplicationInstantiatedTypesFinder")
+        //     );
         
 
         // Apply additional configurations from the Scala script
-        config = config
-                .withValue("org.opalj.fpcf.analyses.AllocationSiteBasedPointsToAnalysis.mergeStringConstants", ConfigValueFactory.fromAnyRef(false))
-                .withValue("org.opalj.fpcf.analyses.AllocationSiteBasedPointsToAnalysis.mergeClassConstants", ConfigValueFactory.fromAnyRef(false));
+        // config = config
+        //         .withValue("org.opalj.fpcf.analyses.AllocationSiteBasedPointsToAnalysis.mergeStringConstants", ConfigValueFactory.fromAnyRef(false))
+        //         .withValue("org.opalj.fpcf.analyses.AllocationSiteBasedPointsToAnalysis.mergeClassConstants", ConfigValueFactory.fromAnyRef(false));
 
-        config = config
-                .withValue("org.opalj.fpcf.analyses.cg.reflection.ReflectionRelatedCallsAnalysis.highSoundness", ConfigValueFactory.fromAnyRef(false));
+        // config = config
+        //         .withValue("org.opalj.fpcf.analyses.cg.reflection.ReflectionRelatedCallsAnalysis.highSoundness", ConfigValueFactory.fromAnyRef(false));
         
-        config = config.withValue(
-            "org.opalj.tac.fpcf.analyses.pointsto.TamiFlex.logFile",
-            ConfigValueFactory.fromAnyRef(tamiflexLogPath)
+                
+                
+                
+                // tamiflex configs
+        if (use_tamiflex) {
+
+            baseConfig = baseConfig.withValue(
+                "org.opalj.tac.fpcf.analyses.pointsto.TamiFlex.logFile",
+                ConfigValueFactory.fromAnyRef(tamiflexLogPath)
+            );
+                
+            // config = config.withValue(
+            //     "org.opalj.tac.fpcf.analyses.pointsto.TamiFlex.logFile",
+            //     ConfigValueFactory.fromAnyRef(tamiflexLogPath)
+            // );
+            // List<String> tamiflexSchedulers = Arrays.asList(
+            //     "org.opalj.tac.fpcf.analyses.cg.reflection.TamiFlexCallGraphAnalysisScheduler"
+            // );
+
+            // 2. Fetch the existing active modules using the CORRECT config key
+            // List<String> existingModules = config.getStringList("org.opalj.tac.cg.CallGraphKey.modules");
+
+            // 3. Combine them
+            // List<String> updatedModules = new ArrayList<>(existingModules);
+            // List<String> updatedModules = new ArrayList<>();
+            // updatedModules.addAll(tamiflexSchedulers);
+
+            // 4. Overwrite the module list in your config
+            // config = config.withValue(
+            //     "org.opalj.tac.cg.CallGraphKey.modules",
+            //     ConfigValueFactory.fromIterable(updatedModules)
+            // );
+
+        }   
+        
+        // LogContext projectLogContext = GlobalLogContext.successor();
+
+        // // Project<?> project = Project.apply(projectJar, projectLogContext, config);
+        // Project<?> project = Project.apply(appFiles, libFiles, projectLogContext, config);
+        LogContext projectLogContext = new StandardLogContext();
+        OPALLogger$.MODULE$.register(
+            projectLogContext, 
+            new ConsoleOPALLogger(false, Info$.MODULE$) 
         );
-        
-        LogContext projectLogContext = GlobalLogContext.successor();
-
-        // Project<?> project = Project.apply(projectJar, projectLogContext, config);
-        Project<?> project = Project.apply(appFiles, libFiles, projectLogContext, config);
+        // Project<?> project = Project.apply(appFiles, libFiles, projectLogContext, config);
+        Project<?> project = Project.apply(appFiles, libFiles, projectLogContext, baseConfig);
 
         try {
             writeCallGraph(project, algorithm, new File(output));
